6478fa56b3353741a8023421c60b59e7a0ef8995
Jorge Luis Pena <jluispena@gips.local>
2016-10-19 09:27:58 -0500

    Initial commit

---
+++ app/src/main/java/com/example/jluispena/fblasale/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.jluispena.fblasale;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import static android.R.attr.button;
+
+public class MainActivity extends AppCompatActivity {
+        private static Button button;
+        @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        }
+
+    public void OpenBuyActivity(View v){
+
+        setContentView(R.layout.activity_buy);
+    }
+    public void OpenSellActivity(View v){
+
+        setContentView(R.layout.activity_sell);
+    }
+    public void OpenDonateActivity(View v){
+        setContentView(R.layout.activity_donate);
+    }
+}
