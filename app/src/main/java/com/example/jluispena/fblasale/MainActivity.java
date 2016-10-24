d33229ac1889f71c720e621e6f3c42c64c871001
jluispena <jluispena@gips.org>
2016-10-24 09:39:57 -0500

    I added the back button to go back to the main page

--- app/src/main/java/com/example/jluispena/fblasale/MainActivity.java
+++ app/src/main/java/com/example/jluispena/fblasale/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.jluispena.fblasale;
 
 import android.content.Intent;
+import android.graphics.Camera;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
