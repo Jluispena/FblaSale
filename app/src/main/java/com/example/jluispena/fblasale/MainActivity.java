ac0a524aec8cc307091df83713c02e9d034dcb45
jluispena <jluispena@gips.org>
2016-11-07 09:00:06 -0600

    I added the back button to go back to the main page

--- app/src/main/java/com/example/jluispena/fblasale/MainActivity.java
+++ app/src/main/java/com/example/jluispena/fblasale/MainActivity.java
@@ -1,13 +1,10 @@
 package com.example.jluispena.fblasale;
 
 import android.content.Intent;
-import android.graphics.Camera;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-
-import static android.R.attr.button;
 
 public class MainActivity extends AppCompatActivity {
         private static Button button;
@@ -16,19 +13,24 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         }
-
     public void OpenBuyActivity(View v){
-
-        setContentView(R.layout.activity_buy);
+        Intent intent = new Intent(this, BuyActivity.class);
+        startActivity(intent);
     }
     public void OpenSellActivity(View v){
-
-        setContentView(R.layout.activity_sell);
+        Intent intent = new Intent(this, SellActivity.class);
+        startActivity(intent);
     }
     public void OpenDonateActivity(View v){
-        setContentView(R.layout.activity_donate);
+        Intent intent = new Intent(this, DonateActivity.class);
+        startActivity(intent);
     }
-    public void OpenMainActivity(View v){
-        setContentView(R.layout.activity_main);
+    public void BuyActivity(View v){
+       setContentView(R.layout.activity_buy);
+
+    }
+    public void OpenScrollingActivity(View v) {
+        Intent intent = new Intent(this, ScrollingActivity.class);
+        startActivity(intent);
     }
 }
